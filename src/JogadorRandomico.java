609a30446cf02742df366db3bfcbb8bea4bef860
thayckowisk <thayckowisk@hotmail.com>
2023-08-10 18:14:07 -0300

    fixed principal

--- src/JogadorRandomico.java
+++ src/JogadorRandomico.java
@@ -1,6 +1,13 @@
 public class JogadorRandomico extends JogadorAbstrato {
+    private Baralho monte;
+
     public JogadorRandomico(String nome) {
         super(nome);
+
     }
 
+    public void jogarTurno() {
+        monte.pegarDoTopo();
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,12 @@
 public class Principal {
     public static void main(String[] args) {
+    // System.out.println("###### ~ SUPER TRUNFO ~ ######");
+    // System.out.println("Bem vinde ao jogo!");
+    // System.out.println("Para escolher o tema Aviões, digite Aviões!");
+    // System.out.println("Para escolher o tema Carros, digite Carros!");
+    // System.out.println("Para escolher o tema Dinossauros, digite Dinossauros!");
+    // System.out.println("Para escolher o tema Heróis, digite Heróis!");
+
         JogadorReal jogador1 = new JogadorReal("João");
         JogadorReal jogador2 = new JogadorReal("Maria");
         JogadorRandomico jogador3 = new JogadorRandomico("Tnay");
@@ -7,5 +14,6 @@ public class Principal {
          Baralho superTrunfo = new Baralho("Aviões");
         Jogo rodada01 = new Jogo(jogadores,superTrunfo);
         rodada01.jogar(superTrunfo);
+
     }
 }
