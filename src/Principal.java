20eccbee08607fef624cd2b4b5c2583e58070079
Júlia Oliveira <juliatoddy@gmail.com>
2023-08-25 00:55:56 -0300

    Implementação do Menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,58 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
-    // System.out.println("###### ~ SUPER TRUNFO ~ ######");
-    // System.out.println("Bem vinde ao jogo!");
-    // System.out.println("Para escolher o tema Aviões, digite Aviões!");
-    // System.out.println("Para escolher o tema Carros, digite Carros!");
-    // System.out.println("Para escolher o tema Dinossauros, digite Dinossauros!");
-    // System.out.println("Para escolher o tema Heróis, digite Heróis!");
+        Scanner scanner = new Scanner(System.in);
+        String tema = "";
+
+        try {
+            int escolha = 5;
+            boolean valido = false;
+
+            while (!valido) {
+                try {                
+                    while (escolha < 1 || escolha > 4){
+                        System.out.println("###### ~ SUPER TRUNFO ~ ######");
+                        System.out.println("Bem vindo ao jogo!");
+                        System.out.println("Para escolher o tema Aviões, digite 1");
+                        System.out.println("Para escolher o tema Carros, digite 2");
+                        System.out.println("Para escolher o tema Dinossauros, digite 3");
+                        System.out.println("Para escolher o tema Heróis, digite 4");
+                        
+                        escolha = scanner.nextInt();
+                    }
+
+                    valido = true;
+                    
+                    if (escolha == 1) {
+                        tema = "Aviões";
+                    }
+                    else if (escolha == 2) {
+                        tema = "Carros";
+                    }
+                    else if (escolha == 3) {
+                        tema = "Dinossauros";
+                    }
+                    else {
+                        tema = "Heróis";
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Digite um número válido!");
+                    scanner.next();
+                }
+            }
+        }
+        finally {
+            scanner.close();
+        }
+        
+        Baralho superTrunfo = new Baralho(tema);
 
         JogadorReal jogador1 = new JogadorReal("João");
-        JogadorReal jogador2 = new JogadorReal("Maria");
-        JogadorRandomico jogador3 = new JogadorRandomico("Tnay");
-        JogadorAbstrato[] jogadores = {jogador1,jogador2,jogador3};
-         Baralho superTrunfo = new Baralho("Aviões");
+        JogadorRandomico jogador2 = new JogadorRandomico("Joaquina");
+        JogadorAbstrato[] jogadores = {jogador1, jogador2};
+        
         Jogo rodada01 = new Jogo(jogadores,superTrunfo);
         rodada01.iniciarPartida(superTrunfo);
         rodada01.mostrarStatus();
